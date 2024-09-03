4a5936e37fbadaf31f3e95a019abc19534f81407
Rostik <rostikpahadi@gmail.com>
2024-09-03 22:16:57 +0545

    Adding time and author to newsapp

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,15 +3,16 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
 
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+    let {title,description,imageUrl,newsUrl,author,date} = this.props;
     return (
      
       <div className="my-3">
           <div className="card">
-            <img src={imageUrl} className="card-img-top " alt="..."/>
+         <img src={imageUrl} className="card-img-top " alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
+              <h5 className="card-title">{title}  </h5>
               <p className="card-text">{description}</p>
+              <p className="card-text"><small className=" text-muted">By {author} on {new Date(date).toGMTString()}</small></p>
               <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
