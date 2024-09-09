ebba8eeaef4b2f0ec68edfdcc5c8d4a695c757f6
Rostik <rostikpahadi@gmail.com>
2024-09-09 20:28:54 +0545

    Adding top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -37,16 +37,20 @@ export default class News extends Component {
        this.Update();
    } 
     async Update(){
+      this.props.setProgress(20);//it invokes the setProgress method passed down from the parent (App) component.
       const url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=803aabb7d4744bcfb978fdd0a50f8461&page=${this.state.page}&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});//It sets loading to true to indicate that data is being fetched.
       let data=await fetch(url);
+      this.props.setProgress(50);
       let parseData=await data.json();
+      this.props.setProgress(80);
       console.log(parseData);
       this.setState({ //Once the data is received and parsed into JSON, the component's state is updated with
         articles:parseData.articles, //An array of the fetched news articles.
         totalResults:parseData.totalResults,
         loading:false  // Set to false to indicate that data loading is complete.
       })
+      this.props.setProgress(100);
 
     }
     fetchMoreData=async()=>{
